7ef3902f7e10a34351ec12362049e7d12a7aafe9
Neil Comber <neilcomber@gmail.com>
2023-10-02 06:35:06 +0100

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UsersList from "./UsersList";
+
+function App() {
+    return (
+        <div className="container mx-auto">
+            <UsersList></UsersList>
+        </div>
+    )
+}   
+
+
+export default App;
