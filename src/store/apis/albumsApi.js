e04c6a750fa9cd8bea9db6eb32d1522e96728462
Neil Comber <neilcomber@gmail.com>
2023-10-04 11:45:53 +0100

    adding photos to album functionality

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -22,7 +22,12 @@ const albumsApi = createApi({
         return {
             fetchAlbums: builder.query({
                 providesTags: (result, error, user) => {
-                    return [{ type: 'Album', id: user.id }];
+                    const tags = result.map(album => {
+                        return { type: 'Album', id: album.id };
+                    });
+                    tags.push({ type: 'UsersAlbums', id: user.id });
+                    return tags;
+                    
                 }, 
                 query: (user) => {
                     return {
@@ -36,7 +41,7 @@ const albumsApi = createApi({
             }), 
             addAlbum: builder.mutation({
                 invalidatesTags: (result, error, user) => {
-                    return [{type: 'Album', id: user.id}]
+                    return [{type: 'UsersAlbums', id: user.id}]
                 },
                 query: (user) => {
                     return {
@@ -48,10 +53,28 @@ const albumsApi = createApi({
                         },
                     }
                 }
+            }), 
+            removeAlbum: builder.mutation({
+                invalidatesTags: (result, error, album) => {
+                    return [{
+                        type: 'Album', 
+                        id: album.id
+                    }];
+                },
+                query: (album) => {
+                    return {
+                        url: `/albums/${album.id}`,
+                        method: 'DELETE',
+                    };
+                }
             })
         }
     }
 });
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export const {
+    useFetchAlbumsQuery,
+    useAddAlbumMutation,
+    useRemoveAlbumMutation
+    } = albumsApi;
 export { albumsApi };
--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,18 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducer } from './slices/usersSlice';
 import { albumsApi } from './apis/albumsApi';
+import { photosApi } from './apis/photosApi';
 
 export const store = configureStore({
     reducer: {
         users: usersReducer, 
-        [albumsApi.reducerPath]: albumsApi.reducer
+        [albumsApi.reducerPath]: albumsApi.reducer, 
+        [photosApi.reducerPath]: photosApi.reducer
     }, 
     middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(albumsApi.middleware);
+        return getDefaultMiddleware()
+            .concat(albumsApi.middleware)
+            .concat(photosApi.middleware);
     }
 });
 
@@ -20,4 +24,13 @@ setupListeners(store.dispatch);
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/deleteUser';
-export { useFetchAlbumsQuery, useAddAlbumMutation } from './apis/albumsApi';
+export {
+    useFetchAlbumsQuery,
+    useAddAlbumMutation,
+    useRemoveAlbumMutation
+} from './apis/albumsApi';
+export {
+    useFetchPhotosQuery,
+    useAddPhotoMutation,
+    useRemovePhotoMutation
+} from './apis/photosApi';
